4426f4bb0e2ef67bdfef7ae16a0b0d1ce372aeaf
Leone0038 <leonedantas80@gmail.com>
2026-03-17 18:03:19 -0300

    fix: remove unnecessary whitespace in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
+    
     return (
         <div className="app-container">
             <Header />

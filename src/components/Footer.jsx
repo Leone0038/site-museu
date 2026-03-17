2fe098d81a3a8f7d742adf60d77cba9f4a661439
Leone0038 <leonedantas80@gmail.com>
2026-03-17 18:03:28 -0300

    feat: add logo image with click redirect to home in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,24 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Footer() {
+    const navigate = useNavigate();
+    function redirectToHome() {
+        navigate("/");
+    }
     return (
         <footer className="footer">
-            <p className="year">© {new Date().getFullYear()} Museu Regional de Canindé</p>
+            <img
+                src="/assets/museum-logo.svg"
+                alt="Logo do museu"
+                className="museum-logo"
+                onClick={redirectToHome}
+                width={100}
+                loading="eager"
+                fetchpriority="high"
+            />
+            <p className="year">
+                © {new Date().getFullYear()} Museu Regional de Canindé
+            </p>
         </footer>
     );
 }

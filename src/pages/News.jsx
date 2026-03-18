3bfbc11bafcb991e4c6220fa9c26f1200362809e
Leone0038 <leonedantas80@gmail.com>
2026-03-17 21:50:00 -0300

    fix: update News component to include news-container class for improved styling

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 
 export default function News() {
     return (
-        <div className="app-container">
+        <div className="app-container news-container">
             <Header />
             <Footer />
         </div>
